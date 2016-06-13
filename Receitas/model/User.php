daf1ba89341b254a0220a4e79f598ade846fd208
Gustavo Garcia <gustavo.gonzatto11@gmail.com>
2016-06-13 10:39:53 -0300

    Correção

--- Receitas/model/User.php
+++ Receitas/model/User.php
@@ -2,21 +2,47 @@
 class User
 {
 	private $name;
-	private $last_name;
+	private $lastName;
 	private $email;
 	private $birthdate;
 	private $phone;
 	private $password;
-	public function __construct($name, $last_name,
+	public function __construct($name, $lastName,
 	$email, $birthdate, $phone, $password)
 	{
 		$this->name = $name;
-		$this->last_name = $last_name;
+		$this->last_name = $lastName;
 		$this->email = $email;
 		$this->birthdate = $birthdate;
 		$this->phone = $phone;
 		$this->password = $password;
 	}
+
+	private function setName ($name){
+		$this->name = $name;
+	}
+	
+	private function setLastName ($lastName){
+		$this->lastName = $lastName;
+	}
+	
+	private function setEmail ($email){
+		$this->email = $email;
+	}
+	
+	private function setBirthDate ($birthdate){
+		$this->birthdate = $birthdate;
+	}
+	
+	private function setPhone($phone){
+		$this->phone = $phone;
+	}
+	
+	private function setPassword($password){
+		$this->password = $password;
+	}
+	
+	
 	public function getName()
 	{
 		return $this->name;
